0e3719514b29f90b65e7973d93b13eaf119f0a22
edwin1028 <edwinbermejo.jr@gmail.com>
2023-11-18 11:55:42 +0800

    set datasource with env

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 6543,
-    username: "postgres",
-    password: "supersecretpassword",
-    database: "dbsample",
+    host: process.env.DB_HOST,
+    port: parseFloat(process.env.DB_PORT as string),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: (process.env.NODE_ENV as string) === "development",
     logging: (process.env.NODE_ENV as string) === "development",
     entities: [User],
