0e36e8e891203e12e920118dc77008b72d8e3de5
edwin1028 <edwinbermejo.jr@gmail.com>
2023-11-19 09:37:38 +0800

    Login form

--- frontend/src/pages/Landing/Login/LoginPage.tsx
+++ frontend/src/pages/Landing/Login/LoginPage.tsx
@@ -1,5 +1,81 @@
+import {
+    Box,
+    Button,
+    Paper,
+    TextField,
+    Typography,
+    colors,
+} from "@mui/material";
+
 const LoginPage = () => {
-    return <p>Login works!</p>;
+    return (
+        <Box
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            sx={{
+                height: "100%",
+                width: "100%",
+            }}
+        >
+            <Paper elevation={3}>
+                <Box
+                    sx={(theme) => ({
+                        [theme.breakpoints.up("sm")]: {
+                            width: "500px",
+                            px: "32px",
+                            py: "48px",
+                        },
+                    })}
+                >
+                    <Box sx={{ textAlign: "center", mb: 3 }}>
+                        <Typography variant="h4">Log in</Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                mt: 1,
+                                color: colors.grey[500],
+                            }}
+                        >
+                            Sign in with your credentials.
+                        </Typography>
+                    </Box>
+                    <TextField
+                        sx={{
+                            my: 2,
+                        }}
+                        fullWidth
+                        label="Email"
+                        variant="outlined"
+                        type="email"
+                    />
+                    <TextField
+                        sx={{
+                            my: 2,
+                        }}
+                        fullWidth
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                    />
+
+                    <Button
+                        sx={{
+                            mt: 5,
+                            fontSize: 18,
+                            textTransform: "none",
+                        }}
+                        fullWidth
+                        variant="contained"
+                        size="large"
+                    >
+                        Log in
+                    </Button>
+                </Box>
+            </Paper>
+        </Box>
+    );
 };
 
 export default LoginPage;
